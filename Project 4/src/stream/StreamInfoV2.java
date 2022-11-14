f416d214b8a979281fbe195b0435083db93113e1
Kaium Al Limon <116227589+limon221291@users.noreply.github.com>
2022-11-15 01:04:48 +0600

    Update StreamInfoV2.java

--- Project 4/src/stream/StreamInfoV2.java
+++ Project 4/src/stream/StreamInfoV2.java
@@ -3,7 +3,7 @@ package stream;
 public class StreamInfoV2 extends StreamInfo{
     public void watchMovie(){
         super.updateBalance(100);
-        super.updateMovieNumber(1);
+        super.updateMovieNumber(2);
     }
 }
 
